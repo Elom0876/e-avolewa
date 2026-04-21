4078aba34719757da12bbb0f235825bf17114cd0
Elom0876 <pierredekadjevi@gmail.com>
2026-04-21 16:41:15 +0100

    Fix SSL options MySQL

--- config/database.php
+++ config/database.php
@@ -59,10 +59,12 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => [
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', ''),
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-            ],
+                PDO::MYSQL_ATTR_SSL_KEY => null,
+                PDO::MYSQL_ATTR_SSL_CERT => null,
+            ]) : [],
         ],
 
         'mariadb' => [
